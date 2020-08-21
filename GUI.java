d261464e4bfc438094e4ffc63f31c336f30c170f
Rahul Ratwatte <racr20@bath.ac.uk>
2020-08-21 20:27:01 +0100

    Added GUI class

---
+++ GUI.java
@@ -0,0 +1,21 @@
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+
+public class GUI {
+
+    public GUI() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 10));
+        panel.setLayout(new GridLayout(0,1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Connect Four");
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args){// main method 
 
+        new GUI();
+
         MyConnectFour myConnectFour = new MyConnectFour();//creation of a MyConnectFour object
 
         myConnectFour.playGame();//playGame called to initiate the connect four game
